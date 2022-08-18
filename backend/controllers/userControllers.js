fffb267a3456defd6010e51cbd733a347e6411cb
mullaivanaraj <mullaivanaraj@gmail.com>
2022-08-18 10:43:34 +0530

    github activity check

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -47,6 +47,7 @@ const authUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id)
     });
   } 
+  
   else {
     res.status(401);
     throw new Error('Invalid credentials')
