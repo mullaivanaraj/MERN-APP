f5b5bc8c7b68e5a7db572ea73a0f9b27c83ae1bc
mullaivanaraj <mullaivanaraj@gmail.com>
2022-08-18 23:51:38 +0530

    added error handling middleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
   name: { type: String, trim: true, required: true },
   email: { type: String, trim: true, required: true, unique: true },
   password: { type: String, trim: true, required: true },
-  picture: { type: String, trim: true, default:
+  pic: { type: String, trim: true, default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg", },
 },
 {
